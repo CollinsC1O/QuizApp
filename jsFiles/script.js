5430aabb4b43a5ebc8d65c8fa67bab2e9f411188
collinsC10 <collins4chinonso@gmail.com>
2024-08-26 09:01:27 +0100

    feat: implement question structure js

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -2,7 +2,7 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const quiz_box = document.querySelector(".quiz_box");
 const exit_btn = document.querySelector(".buttons .quit");
-const continue_btn = document.querySelector(".buttons .restart");
+const continue_btn = info_box.querySelector(".buttons .restart");
 
 //the start quiz button click to start
 start_btn.onclick = () => {
@@ -12,4 +12,10 @@ start_btn.onclick = () => {
 //Exit button clicked to exit quiz and return back to the start button
 exit_btn.onclick = () => {
     info_box.classList.remove("activeInfo") //hides the info box
+}
+
+//continue button click to continue to the quiz box
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo") //hides the info box
+    quiz_box.classList.add("activeQuiz") //display quiz box
 }
