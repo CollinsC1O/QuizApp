a12665388a31c06e65f4298c9673c8e71f966ba2
collinsC10 <collins4chinonso@gmail.com>
2024-08-26 15:32:54 +0100

    feat: add questions

--- jsFiles/questions.js
+++ jsFiles/questions.js
@@ -2,12 +2,123 @@
 let questions = [
     {
         numb: 1,
-        question: "",
+        question: "What is Scarb?",
+        answer: "Cairo's package manager and build system",
         option: [
-            "",
-            "",
-            "",
-            ""
+            "Cairo code tester",
+            "Cairo editor",
+            "Cairo's package manager and build system",
+            "Starknet build system"
         ]
-    }
+    },
+    {
+        numb: 2,
+        question: "What is the name of the initial function that a Cairo program runs?",
+        answer: "main",
+        option: [
+            "mian",
+            "function",
+            "made",
+            "main"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which command will run the code in your project?",
+        answer: "scarb cairo-run",
+        option: [
+            "scarb run",
+            "scarb cairo-run",
+            "run scarb",
+            "scarb code-run"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which syntax creates a variable that can be reassigned?",
+        answer: "let mut x = 99",
+        option: [
+            "let y = 10",
+            "const MINUTES_PER_HOUR = 60",
+            "var j = 12",
+            "let mut x = 99"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The keyword for declaring a new function in Cairo is:",
+        answer: "fn",
+        option: [
+            "function",
+            "func",
+            "fn",
+            "fn_"
+        ]
+    },
+    {
+        numb: 6,
+        question: "In Cairo, a curly-brace block like `{ /* ... */ }` is:",
+        answer: "An expression and A syntactic scope",
+        option: [
+            "An expression",
+            "A syntactic scope",
+            "A statement",
+            "An expression and A syntactic scope"
+        ]
+    },
+    {
+        numb: 7,
+        question: "cairo can be used in write provable programs",
+        answer: "true",
+        option: [
+            "false",
+            "true",
+            "maybe",
+            "who knows"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the syntax introducing a comment in cairo?",
+        answer: "//",
+        option: [
+            "\\",
+            "/-/",
+            "//",
+            "<>"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What happens in Cairo if an if condition evaluates to true?",
+        answer: "The program executes the block of code associated with the if condition",
+        option: [
+            "The program terminates immediately.",
+            "The program executes the block of code associated with the if condition",
+            "The program skips the if block and executes the next block of code.",
+            "nothing happens"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the result of running a Cairo program with an if statement where the condition evaluates to false and no else block is provided?",
+        answer: "The program skips the if block and moves on to the next bit of code.",
+        option: [
+            "The program skips the if block and moves on to the next bit of code.",
+            "The program panics.",
+            "The program executes an implicit else block.",
+            "The program stops execution."
+        ]
+    },
+    {
+        numb: 11,
+        question: "What feature does Cairo provide for repeating code execution until a specified condition is met or gas runs out?",
+        answer: "`loop` or `while` with a break condition.",
+        option: [
+            "Traditional `for` loops similar to those in imperative languages.",
+            "Automated mechanisms that handle execution retries for you.",
+            "`loop` or `while` with a break condition.",
+            "repeat"
+        ]
+    },
 ]
